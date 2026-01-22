813b7cd1e297542f178a3b10f2d423d7649e513f
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2026-01-22 16:16:11 +0900

    [project_research] update

--- project_research/teleop/baseline_bag.sh
+++ project_research/teleop/baseline_bag.sh
@@ -4,4 +4,4 @@ rosbag record -b 16384 --chunksize 1024 \
 /divided_robot/gripper_controller/state \
 /divided_robot/trajectory_controller/state \
 /divided_robot/joint_states \
-/usb_cam/image_raw
+/Camera0/color/image_raw
